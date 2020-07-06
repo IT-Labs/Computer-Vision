310c220ea89c703af7a5ab08b43774f86e00f749
Vlatko Zmejkoski <vlatko.zmejkoski@it-labs.com>
2020-07-06 21:23:27 +0200

    Updated download_images.py to be able to work on any OS paths.

--- captcha_breaker/download_images.py
+++ captcha_breaker/download_images.py
@@ -1,8 +1,11 @@
 from selenium import webdriver
 from time import sleep
 import argparse
+import os
 
-pathToChromeDriver = r"path to chrome driver exe"
+currDir = os.path.dirname(__file__)
+pathToChromeDriver = os.path.join(currDir, 'chromedriver')
+url = r"https://enroll-eca-eco-intg.educationpartners.com/Account/Register/"
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-n", "--num-images", type=int, default=500, help="# of images to download")
@@ -13,10 +16,9 @@ options.add_argument("--start-maximized")
 driver = webdriver.Chrome(pathToChromeDriver, chrome_options=options)
 
 for i in range(0, args["num_images"]):
-
-    driver.get('path to site')
+    driver.get(url)
     sleep(1)
-    pathToSaveThePicture = r"path where you want to save the picture" + str(i) + ".png"
+    pathToSaveThePicture = os.path.join(currDir, 'captcha_images', str(i) + ".png")
     image = driver.find_element_by_css_selector('img[id=CaptchaImage]').screenshot(pathToSaveThePicture)
     sleep(0.2)
 
